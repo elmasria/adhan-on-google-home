3dd77963af4558a6fae49dbae7ea3388af34979f
Ahmad El Masri <ahmadjelmasri@gmail.com>
2023-06-23 12:47:32 +0200

    feat: add flask routes

---
+++ app/flask_app.py
@@ -0,0 +1,27 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, jsonify
+
+
+def init_app(scheduler: BackgroundScheduler):
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return jsonify({"status": "good"}), 200
+
+    @app.route("/scheduler")
+    def scheduler_route():
+        jobs = scheduler.get_jobs()
+        if not jobs:
+            return jsonify({"message": "No jobs scheduled"})
+        result = []
+        for job in jobs:
+            job_details = {
+                "job_id": job.id,
+                "job_function": str(job.func),
+                "next_run_time": str(job.next_run_time),
+            }
+            result.append(job_details)
+        return jsonify(result)
+
+    return app
