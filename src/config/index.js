a16ba38cb98278f5e8412ed5a8e92b933d85a836
Ahmad El Masri <ahmadjelmasri@gmail.com>
2023-06-23 08:02:12 +0200

    chore: remove js files

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ATHAN: {
-        DEFAULT: "https://amasri.com/athan.mp3",
-        FAJER: "https://amasri.com/athan/athan-fajer.mp3",
-        ASR: "https://amasri.com/athan/athan-asr.mp3",
-        MAGHRIB: "https://amasri.com/athan/athan-maghrib.mp3",
-        ISHA: "https://amasri.com/athan/athan-isha.mp3"
-    }
-}
