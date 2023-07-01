1d2b00f0dfe460abfe451227cd914ca3f50d13a1
Ahmad El Masri <ahmadjelmasri@gmail.com>
2023-07-01 12:43:12 +0200

    fix: config env

--- app/config.py
+++ app/config.py
@@ -12,9 +12,9 @@ def get_app_config() -> IAppConfig:
         "mawaqit": {
             "username": os.getenv("MAWAQIT_USERNAME", None),
             "password": os.getenv("MAWAQIT_PASSWORD", None),
-            "mosque_uuid": os.getenv("DEVICE_NAME", None),
+            "mosque_uuid": os.getenv("MAWAQIT_MOSQUE", None),
         },
-        "device_name": os.getenv("MAWAQIT_MOSQUE", None),
+        "device_name": os.getenv("DEVICE_NAME", None),
     }
 
     return app_config
